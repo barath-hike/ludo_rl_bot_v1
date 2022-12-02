2672906f2af7afa1a52196e06e40c101feb7254f
barath-hike <barath@hike.in>
2022-12-02 06:02:56 +0000

    train code

--- train.py
+++ train.py
@@ -14,6 +14,19 @@ def train():
 
     files = glob.glob("../ludo_rl_bot_v1_data/train_data/*.pkl")
 
+    agent0 = Agent(17, 4)
+    agent1 = Agent(17, 4)
+
+    if os.path.exists('../ludo_rl_bot_v1_data/saved_models/v1/current_model.hdf5'):
+        agent0.load('../ludo_rl_bot_v1_data/saved_models/v1/current_model.hdf5')
+    else:
+        agent0.load_model('./data_model_84.hdf5')
+
+    if os.path.exists('../ludo_rl_bot_v1_data/saved_models/v2/current_model.hdf5'):
+        agent1.load('../ludo_rl_bot_v1_data/saved_models/v2/current_model.hdf5')
+    else:
+        agent1.load_model('./data_model_84.hdf5')
+
     if len(files) > 1:
 
         for file in files:
@@ -46,19 +59,6 @@ def train():
             rew_path1.append(rewards1)
 
             os.system("mv " + file + " ../ludo_rl_bot_v1_data/trained_data/")
-
-        agent0 = Agent(17, 4)
-        agent1 = Agent(17, 4)
-
-        if os.path.exists('../ludo_rl_bot_v1_data/saved_models/v1/current_model.hdf5'):
-            agent0.load('../ludo_rl_bot_v1_data/saved_models/v1/current_model.hdf5')
-        else:
-            agent0.load_model('./data_model_84.hdf5')
-
-        if os.path.exists('../ludo_rl_bot_v1_data/saved_models/v2/current_model.hdf5'):
-            agent1.load('../ludo_rl_bot_v1_data/saved_models/v2/current_model.hdf5')
-        else:
-            agent1.load_model('./data_model_84.hdf5')
 
         agent0.learn(batch, agent0.get_returns(rew_path))
         agent1.learn(batch, agent1.get_returns(rew_path1))
